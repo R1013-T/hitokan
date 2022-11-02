a376ece8d3310d666fae5fe896f9d202ad216c95
R1013-T <nosuke1013@icloud.com>
2022-11-02 16:10:13 +0900

    done

--- components/auth/signup/beforeAuth/Input.tsx
+++ components/auth/signup/beforeAuth/Input.tsx
@@ -1,9 +1,26 @@
+import styles from "../../../../styles/auth/auth.module.scss";
+
+import Link from "next/link";
+
+import { MdArrowBackIosNew } from "react-icons/md";
+
 const Input = () => {
   return (
     <div>
-      Enter
+      <Link href={"/"} className={styles.backButton}>
+        <MdArrowBackIosNew />
+        <p>Back</p>
+      </Link>
+      <form className={styles.signupInputForm}>
+        <h2>CREATE YOUR ACCOUNT</h2>
+        <div className={styles.inner}>
+          <label>メールアドレス</label>
+          <input type="text" placeholder="Email Address" />
+          <button type="submit">Continue</button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default Input;
